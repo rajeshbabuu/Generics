875ca7b92f99f3e5db5bcbd429c613bc4615cb25
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-11-29 23:34:31 +0530

    Sorting Method

--- Generics/Generics/GenericsProgram.cs
+++ Generics/Generics/GenericsProgram.cs
@@ -6,33 +6,35 @@ using System.Threading.Tasks;
 
 namespace Generics
 {
-    public class MaxNumberCheck<T> where T : IComparable
+    public class GenericMaximum<T> where T : IComparable
     {
-        public T val1, val2, val3;
-
-        public MaxNumberCheck(T val1, T val2, T val3)
+        public T[] value;
+        public GenericMaximum(T[] value)
         {
-            this.val1 = val1;
-            this.val2 = val2;
-            this.val3 = val3;
+            this.value = value;
         }
 
-        public static T MaxIntegerNumber(T val1, T val2, T val3)
+        public T[] Sort(T[] values)
         {
-            if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0 || val1.CompareTo(val2) >= 0 && val1.CompareTo(val3) > 0 || val1.CompareTo(val2) > 0 && val1.CompareTo(val3) >= 0)
-            {
-                return val1;
-            }
-            if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0 || val2.CompareTo(val1) >= 0 && val2.CompareTo(val3) > 0 || val2.CompareTo(val1) > 0 && val2.CompareTo(val3) >= 0)
-            {
-                return val2;
-            }
+            Array.Sort(values);
+            return values;
+        }
 
-            if (val3.CompareTo(val1) > 0 && val3.CompareTo(val1) > 0 || val3.CompareTo(val1) >= 0 && val3.CompareTo(val1) > 0 || val3.CompareTo(val1) > 0 && val3.CompareTo(val1) >= 0)
-            {
-                return val3;
-            }
-            return val1;
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[^1];
+        }
+
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum value is " + max);
         }
     }
 }
--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -7,15 +7,10 @@
             Console.WriteLine("-------- Welcome To Generics Problem ---------");
 
 
-            int intArray = MaxNumberCheck<int>.MaxIntegerNumber(23, 42, 12);
-            Console.WriteLine("\nMax integer number is: " + intArray);
 
-            double output = MaxNumberCheck<double>.MaxIntegerNumber(33.2, 23.4, 12.5);
-            Console.WriteLine("\nMax floating number is: " + output);
-
-            char strArr = MaxNumberCheck<char>.MaxIntegerNumber('A', 'S', 'R');
-            Console.WriteLine("\nMax string is: " + strArr);
-
+            int[] arr = { 112, 344, 432, 555, 678 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(arr);
+            generic.PrintMaxValue();
 
 
 
