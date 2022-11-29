0a501f7a30aa6d10799be0e452721a3df4a87bca
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-11-29 23:01:54 +0530

    Print array of Interger, Double and Character

---
+++ Generics/Generics/GenericsProgram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class GenericsProgram
+    {
+        public static void toPrint(int[] inputArray)
+        {
+            foreach (int element in inputArray)
+            {
+                System.Console.WriteLine(element);
+            }
+            Console.WriteLine("------------------------  ");
+        }
+
+        public static void toPrint(double[] inputArray)
+        {
+            foreach (double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("------------------------ ");
+        }
+
+        public static void toPrint(char[] inputArray)
+        {
+            foreach (double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("------------------------");
+        }
+    }
+}
