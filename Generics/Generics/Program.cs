f5c40c0774e9d0e1b2dcfd98adae57e5549b16cf
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-11-29 23:05:40 +0530

    Print array of Interger, Double and Character Using Generics Method

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("-------- Welcome To Generics Problem ---------");
+
+
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H', 'S', 'E', 'L', 'L', 'O' };
+
+            GenericsProgram.toPrint(intArray);
+            GenericsProgram.toPrint(doubleArray);
+            GenericsProgram.toPrint(charArray);
         }
     }
 }
