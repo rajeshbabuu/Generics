3ef5ef6fc46ba891e1d4d3089bbaa074931e56bc
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-11-29 23:30:18 +0530

    Generics class to find maximum int,double and character

--- Generics/Generics/GenericsProgram.cs
+++ Generics/Generics/GenericsProgram.cs
@@ -6,48 +6,18 @@ using System.Threading.Tasks;
 
 namespace Generics
 {
-    public class MaxNumberCheck
+    public class MaxNumberCheck<T> where T : IComparable
     {
-        // integer type
-        public static int MaxIntegerNumber(int val1, int val2, int val3)
-        {
-            if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0 || val1.CompareTo(val2) >= 0 && val1.CompareTo(val3) > 0 || val1.CompareTo(val2) > 0 && val1.CompareTo(val3) >= 0)
-            {
-                return val1;
-            }
-            if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0 || val2.CompareTo(val1) >= 0 && val2.CompareTo(val3) > 0 || val2.CompareTo(val1) > 0 && val2.CompareTo(val3) >= 0)
-            {
-                return val2;
-            }
+        public T val1, val2, val3;
 
-            if (val3.CompareTo(val1) > 0 && val3.CompareTo(val1) > 0 || val3.CompareTo(val1) >= 0 && val3.CompareTo(val1) > 0 || val3.CompareTo(val1) > 0 && val3.CompareTo(val1) >= 0)
-            {
-                return val3;
-            }
-            return val1;
+        public MaxNumberCheck(T val1, T val2, T val3)
+        {
+            this.val1 = val1;
+            this.val2 = val2;
+            this.val3 = val3;
         }
 
-        // double type
-        public static double MaxIntegerNumber(double val1, double val2, double val3)
-        {
-            if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0 || val1.CompareTo(val2) >= 0 && val1.CompareTo(val3) > 0 || val1.CompareTo(val2) > 0 && val1.CompareTo(val3) >= 0)
-            {
-                return val1;
-            }
-            if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0 || val2.CompareTo(val1) >= 0 && val2.CompareTo(val3) > 0 || val2.CompareTo(val1) > 0 && val2.CompareTo(val3) >= 0)
-            {
-                return val2;
-            }
-
-            if (val3.CompareTo(val1) > 0 && val3.CompareTo(val1) > 0 || val3.CompareTo(val1) >= 0 && val3.CompareTo(val1) > 0 || val3.CompareTo(val1) > 0 && val3.CompareTo(val1) >= 0)
-            {
-                return val3;
-            }
-            return val1;
-        }
-
-        // char type
-        public static string MaxIntegerNumber(string val1, string val2, string val3)
+        public static T MaxIntegerNumber(T val1, T val2, T val3)
         {
             if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0 || val1.CompareTo(val2) >= 0 && val1.CompareTo(val3) > 0 || val1.CompareTo(val2) > 0 && val1.CompareTo(val3) >= 0)
             {
--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -7,13 +7,13 @@
             Console.WriteLine("-------- Welcome To Generics Problem ---------");
 
 
-            int intArray = MaxNumberCheck.MaxIntegerNumber(23, 42, 12);
+            int intArray = MaxNumberCheck<int>.MaxIntegerNumber(23, 42, 12);
             Console.WriteLine("\nMax integer number is: " + intArray);
 
-            double output = MaxNumberCheck.MaxIntegerNumber(33.2, 23.4, 12.5);
+            double output = MaxNumberCheck<double>.MaxIntegerNumber(33.2, 23.4, 12.5);
             Console.WriteLine("\nMax floating number is: " + output);
 
-            string strArr = MaxNumberCheck.MaxIntegerNumber("Rajesh", "ViratKholi", "MSDhoni");
+            char strArr = MaxNumberCheck<char>.MaxIntegerNumber('A', 'S', 'R');
             Console.WriteLine("\nMax string is: " + strArr);
 
 
